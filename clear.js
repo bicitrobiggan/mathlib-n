b9ac16f8085802ee720d4f31095d3ce55ce29a81
bicitrobiggan <68156215+bicitrobiggan@users.noreply.github.com>
2022-05-01 16:47:10 +0600

    Add files via upload

--- clear.js
+++ clear.js
@@ -1,15 +1,46 @@
 /*
 Title : 
 Author : Maruf Hasan
-Description :
-Date : , 2021
+Description : clear
+Date : jan 10 , 2022
 */
 
 //dependencies
-const main = require('./handelar/index.js');
+const fs = require("fs");
+let moods = JSON.parse(
+  fs.readFileSync(`${__dirname}/handelar/moods/mood.json`, "utf-8")
+);
+try {
+  JSON.parse(fs.readFileSync(`${__dirname}/.mathLib/user.json`, "utf-8"));
+} catch {
+  fs.unlinkSync(`${__dirname}/.mathLib/user.json`);
+  fs.writeFileSync(`${__dirname}/.mathLib/user.json`, JSON.stringify({}));
+}
+let record = fs.readdirSync(__dirname + "/.record/");
+let trush = fs.readdirSync(__dirname + "/.record/.trush/");
+let restore = fs.readdirSync(__dirname + "/.record/.restored/");
+
+mathlib = function () {
+  let mathlib = ` __  __       _   _     _      _ _ \n|  \\/  |     | | | |   | |    (_) |\n| \\  / | __ _| |_| |__ | |     _| |__\n| |\\/| |/ _\` | __| '_ \\| |    | | '_ \\\n| |  | | (_| | |_| | | | |____| | |_) |\n|_|  |_|\\__,_|\\__|_| |_|______|_|_.__/\n\nmood : ${
+    moods.mood
+  },  status : ${moods.status},  record : ${record.length - 2},  trush : ${
+    trush.length
+  }, restored: ${restore.length}`;
+  let i = 1;
+  let st = setInterval(styleTheName, 100);
+
+  function styleTheName() {
+    let im = mathlib.substring(0, i);
+    console.clear();
+    console.log(im);
+    if (i < mathlib.length) {
+      i++;
+    } else {
+      i = mathlib.length - 1;
+      clearInterval(st);
+    }
+  }
+};
 
 //main function to export
-main.mathlib();
-
-//export and share
-//module.exports =
+module.exports = mathlib;
--- error.js
+++ error.js
@@ -6,12 +6,18 @@ Date : 5 October, 2021
 */
 
 /*---Error function---*/
-function error(type, place, name, ErrorType = TypeError, customString = '') {
-  if (typeof(type) === 'string' && typeof(place) === 'string') {
-    throw new ErrorType(`There is an error in the ${place} parameter of the math.${name}. Please enter ${type} here! ${customString}`);
+function error(type, place, name, ErrorType = TypeError, customString = "") {
+  let err;
+  if (typeof type === "string" && typeof place === "string") {
+    err = new ErrorType(
+      `There is an error in the ${place} parameter of the ${name}() function of mathlib-n. Please enter ${type} here! ${customString}.If you think that it's a bug you can report this bug here : https://github.com/bicitrobiggan/mathlib-n/issues
+      `
+    );
   } else {
-    throw `There is an error in your input!`;
+    err = `There is an error in your input!`;
   }
+  //console.error(errorData);
+  throw err;
 }
 //export amd share
 module.exports = error;
